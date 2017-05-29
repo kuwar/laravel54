29be2d12caeec03684794a21440f68937cce9e8d
saurav <es.developer12@gmail.com>
2017-05-29 15:36:32 +0545

    site setting migration and seed added

---
+++ database/migrations/2017_05_29_093719_create_site_settings_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSiteSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('site_settings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('site_name');
+            $table->string('contact_email');
+            $table->string('contact_phone');
+            $table->string('contact_address');
+            $table->string('facebook_page_url');
+            $table->string('twitter_page_url');
+            $table->string('youtube_page_url');
+            $table->string('googleplus_page_url');
+            $table->string('default_page_title');
+            $table->string('default_meta_keys');
+            $table->string('default_meta_description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('site_settings');
+    }
+}
---
+++ database/seeds/SiteSettingTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SiteSettingTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('site_settings')->insert([
+            'site_name' => str_random(10),
+            'contact_email' => str_random(10).'@gmail.com',
+            'contact_phone' => '1234567890',
+            'contact_address' => '1234567890',
+            'facebook_page_url' => '1234567890',
+            'twitter_page_url' => '1234567890',
+            'youtube_page_url' => '1234567890',
+            'googleplus_page_url' => '1234567890',
+            'default_page_title' => '1234567890',
+            'default_meta_keys' => '1234567890',
+            'default_meta_description' => '1234567890',
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
+        ]);
+    }
+}
