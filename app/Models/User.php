f12d66c8b6a24f4415e98f18a254fb150c17ec28
saurav <es.developer12@gmail.com>
2017-05-29 12:00:17 +0545

    profile controller added

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,19 +1,19 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
+    protected $table = 'users';
+
     protected $fillable = [
         'name', 'email', 'password',
     ];
