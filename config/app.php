afdf57dbda7270fc4601882204ce5707ae841c56
saurav <es.developer12@gmail.com>
2017-05-30 16:01:51 +0545

    interventionn added

--- config/app.php
+++ config/app.php
@@ -181,6 +181,8 @@ return [
 
         Collective\Html\HtmlServiceProvider::class,
 
+        Intervention\Image\ImageServiceProvider::class,
+
     ],
 
     /*
@@ -232,6 +234,8 @@ return [
 
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+
+        'Image' => Intervention\Image\Facades\Image::class,
 
     ],
 
