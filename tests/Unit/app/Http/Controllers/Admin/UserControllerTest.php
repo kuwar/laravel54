badf0237afa69871924c6cde910e7012f1ff2a5d
saurav <es.developer12@gmail.com>
2017-05-31 15:43:39 +0545

    test file added

---
+++ tests/Unit/app/Http/Controllers/Admin/UserControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit\app\Http\Controllers\Admin;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class UserControllerTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
